54ed33793261f6cf8a0ffacb07c04ec8b2413d23
Felix Yu <felix@reverielabs.com>
2023-01-30 14:50:32 +0000

    Changed Harpoon mappings, no NT on launch

--- init.lua
+++ init.lua
@@ -1,7 +1,1 @@
 require("felix_settings")
-
-vim.cmd([[
-    " Start NERDTree when Vim is started without file arguments.
-    autocmd StdinReadPre * let s:std_in=1
-    autocmd VimEnter * if argc() == 0 && !exists('s:std_in') | NERDTree | endif
-]])
